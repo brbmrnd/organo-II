7abc2f5077054d4e199b4aa6073ae4f774c34dd7
bárbara miranda <barbaramntmiranda@gmail.com>
2024-07-11 13:40:45 -0300

    feat: finish remove member funcionality based on id

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -3,7 +3,8 @@ import { IoCloseCircle } from "react-icons/io5";
 
 const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
-        <IoCloseCircle size={25} className='deletar' onClick={aoDeletar} />
+        <IoCloseCircle size={25} className='deletar'
+            onClick={() => aoDeletar(colaborador.id)} />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
