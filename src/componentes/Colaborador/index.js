02df810d114cc1bb0a5445b9f50c13b1c902c214
bárbara miranda <barbaramntmiranda@gmail.com>
2024-07-11 11:23:30 -0300

    add initial logic to delete collab funcionality

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,9 @@
 import './colaborador.css'
+import { IoCloseCircle } from "react-icons/io5";
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
     return (<div className="colaborador">
+        <IoCloseCircle size={25} className='deletar' onClick={aoDeletar} />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
